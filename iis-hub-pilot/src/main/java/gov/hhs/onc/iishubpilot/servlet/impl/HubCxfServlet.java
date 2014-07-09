be24e10112985ff2ba1cb5f926747df151b9ea8b
Michal Kotelba <michal.kotelba@esacinc.com>
2014-07-09 11:54:04 -0400

    - Further supports IISHP-4 and IISHP-6. - Modified WS-Addressing configuration in WSDL(s) to be required (via WS-Policy elements). - Modified SoapUI project to read external test HL7 message file(s). - Added favicon static resource processing to CXF servlet.

--- iis-hub-pilot/src/main/java/gov/hhs/onc/iishubpilot/servlet/impl/HubCxfServlet.java
+++ iis-hub-pilot/src/main/java/gov/hhs/onc/iishubpilot/servlet/impl/HubCxfServlet.java
@@ -2,10 +2,14 @@ package gov.hhs.onc.iishubpilot.servlet.impl;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 import java.util.TreeSet;
+import javax.annotation.Nullable;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.lang3.ClassUtils;
 import org.apache.cxf.Bus;
 import org.apache.cxf.transport.servlet.CXFNonSpringServlet;
@@ -17,6 +21,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.core.annotation.AnnotationAwareOrderComparator;
+import org.springframework.http.MediaType;
 import org.springframework.util.StringUtils;
 import org.springframework.web.context.ContextLoader;
 import org.springframework.web.context.WebApplicationContext;
@@ -24,7 +29,12 @@ import org.springframework.web.context.support.WebApplicationContextUtils;
 import org.springframework.web.context.support.XmlWebApplicationContext;
 
 public class HubCxfServlet extends CXFNonSpringServlet implements ApplicationListener<ContextRefreshedEvent> {
-    public final static String INIT_PARAM_VALUE_DELIMS = ",; \t\n";
+    private final static String INIT_PARAM_VALUE_DELIMS = ",; \t\n";
+
+    private final static String FAVICON_EXT = "ico";
+    private final static String FAVICON_PATH = ("/favicon." + FAVICON_EXT);
+    private final static String FAVICON_PATH_STATIC = ("/static/images/iis-hub-pilot-16x16." + FAVICON_EXT);
+    private final static String FAVICON_CONTENT_TYPE = new MediaType("image", "x-icon").toString();
 
     private final static long serialVersionUID = 0L;
 
@@ -47,6 +57,21 @@ public class HubCxfServlet extends CXFNonSpringServlet implements ApplicationLis
         if (this.appContext != null) {
             this.appContext.close();
         }
+    }
+
+    @Override
+    protected void handleRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException {
+        if (Objects.equals(req.getPathInfo(), FAVICON_PATH)) {
+            this.serveStaticContent(req, resp, FAVICON_PATH_STATIC);
+        } else {
+            super.handleRequest(req, resp);
+        }
+    }
+
+    @Nullable
+    @Override
+    protected String getStaticResourceContentType(String resourceExt) {
+        return (!resourceExt.equals(FAVICON_EXT) ? super.getStaticResourceContentType(resourceExt) : FAVICON_CONTENT_TYPE);
     }
 
     @Override
